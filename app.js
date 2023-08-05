dee0a6fc87d6a63b77d804ea12faccccc990763d
cyan-byte <62404444+cyan-byte@users.noreply.github.com>
2023-08-04 22:48:52 -0400

    Battle begins

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ document.querySelector('.playerImage').appendChild(spaceHero);
 
 const enemy = document.createElement("img");
 enemy.src = "images/enemy.gif";
-document.querySelector('.enemyImage').appendChild(enemy)
+document.querySelector('.enemyImage').appendChild(enemy);
+enemy.style.width = ('256px');
 
 
 class Ship {
@@ -14,33 +15,91 @@ class Ship {
     this.firepower = firepower // amount of damage done to hull of target with a successful hit
     this.accuracy = accuracy // chance btw 0 and 1 that ship will hit target
     }
-// method(){
-//     function attack(attacker, defender){
-//         const damage = 
-// }
+
+    attack(target){
+        if(target.hull <= 0){
+            console.log(`${target.shipName} defeated!`);
+            console.log({humanShip, alienShip});
+        }
+        else{
+            console.log(`${target.shipName} took ${damage} damage. Remaining health: ${target.hull}`);
+            console.log({humanShip, alienShip});
+        }
+    }
 }
 class HumanShip extends Ship {
     constructor (shipName, hull, firepower, accuracy){
         super(shipName, hull, firepower, accuracy);
-    }
-    humanHit() {
-        console.log(`The ${this.shipName} has been hit`);
-    }
+    } 
 }
 
 class AlienShip extends Ship {
     constructor (shipName, hull, firepower, accuracy){
         super(shipName, hull, firepower, accuracy);
     }
-    alienHit () {
-        
-    }
 }
+
+// instances
 const humanShip = new HumanShip("USS Assembly", 20, 5, .7); // instance of HumanShip class
 const alienShip = new AlienShip("Alien Ship", Math.floor(Math.random() * 3) + 3, Math.floor(Math.random() * 2) + 2, Math.floor(Math.random() * .2) + .6);
 
-humanShip.humanHit();
-// You could be battling six alien ships each with unique values.
+
+
+humanShip.firepower = humanShip.firepower - alienShip.accuracy; // alienShip's hit
+alienShip.firepower = alienShip.firepower - humanShip.accuracy; // humanShip's hit
+
+humanShip.hull = humanShip.hull - alienShip.firepower; // humanShip's remaining health points
+alienShip.hull = alienShip.hull - humanShip.firepower; // humanShip's remaining health points
+
+function humanHit() {
+    while (humanShip.hull > 0 && alienShip.hull > 0) {
+        humanShip.firepower = humanShip.firepower - alienShip.accuracy;
+        alienShip.firepower = alienShip.firepower - humanShip.accuracy;
+    
+        humanShip.hull = humanShip.hull - alienShip.firepower;
+        alienShip.hull = alienShip.hull - humanShip.firepower;
+        
+    }
+    console.log({humanShip, alienShip});
+
+    
+    if (humanShip.hull > 0) {
+        console.log(`${humanShip.shipName} wins!`);
+        console.log({humanShip, alienShip});
+
+    } else {
+        console.log(`${alienShip.shipName} wins!`);
+        console.log({humanShip, alienShip});
+
+    }
+}
+
+function alienHit() {
+    while (alienShip.hull > 0 && humanShip.hull > 0) {
+        alienShip.firepower = alienShip.firepower - humanShip.accuracy;
+        humanShip.firepower = humanShip.firepower - alienShip.accuracy;
+    
+        alienShip.hull = alienShip.hull - humanShip.firepower;
+        humanShip.hull = humanShip.hull - alienShip.firepower;
+    }
+    
+    if (alienShip.hull > 0) {
+        console.log(`${alienShip.shipName} wins!`);
+    } else {
+        console.log(`${humanShip.shipName} wins!`);
+    }
+}
+
+humanHit()
+alienHit()
+alienHit()
+alienShip.attack(humanShip) // will not run... ?
+
+
+
+
+
+// You could be battling six alien ships, each with unique values.
 
 // Example use of accuracy to determine a hit:
 
